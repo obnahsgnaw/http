c2fc1bdf5db4c660e54385e77e2defcf17fceff8
wangsb <wangsb@efly-cetc.com>
2024-03-30 18:01:26 +0800

    refactor(x): x

--- engine/engine.go
+++ engine/engine.go
@@ -3,11 +3,8 @@ package engine
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/obnahsgnaw/application/pkg/logging/logger"
-	"github.com/obnahsgnaw/application/pkg/logging/writer"
 	"github.com/obnahsgnaw/http/cors"
 	"github.com/obnahsgnaw/http/corsmid"
-	"github.com/obnahsgnaw/http/multiwriter"
 	"io"
 	"net/http"
 	"time"
@@ -16,12 +13,10 @@ import (
 type Config struct {
 	Name           string
 	DebugMode      bool
-	LogDebug       bool
 	AccessWriter   io.Writer
 	ErrWriter      io.Writer
 	TrustedProxies []string
 	Cors           *cors.Config
-	LogCnf         *logger.Config
 	DefFavicon     bool
 }
 
@@ -30,20 +25,6 @@ func New(cnf *Config) (*gin.Engine, error) {
 		gin.SetMode(gin.DebugMode)
 	} else {
 		gin.SetMode(gin.ReleaseMode)
-	}
-	if cnf.AccessWriter == nil {
-		if w, err := newDefAccessWriter(cnf.LogCnf, cnf.LogDebug); err != nil {
-			return nil, err
-		} else {
-			cnf.AccessWriter = w
-		}
-	}
-	if cnf.ErrWriter == nil {
-		if w, err := newDefErrorWriter(cnf.LogCnf, cnf.LogDebug); err != nil {
-			return nil, err
-		} else {
-			cnf.ErrWriter = w
-		}
 	}
 	if cnf.AccessWriter != nil {
 		gin.DisableConsoleColor()
@@ -95,37 +76,4 @@ func New(cnf *Config) (*gin.Engine, error) {
 	}
 
 	return r, nil
-}
-
-func newDefAccessWriter(logCnf *logger.Config, debug bool) (io.Writer, error) {
-	var wts []io.Writer
-	if logCnf != nil {
-		if w, err := logger.NewAccessWriter(logCnf, debug); err == nil {
-			wts = append(wts, w)
-		} else {
-			return nil, err
-		}
-	}
-	if debug {
-		wts = append(wts, writer.NewStdWriter())
-	}
-
-	return multiwriter.New(wts...), nil
-}
-
-func newDefErrorWriter(logCnf *logger.Config, debug bool) (io.Writer, error) {
-	var wts []io.Writer
-	if logCnf != nil {
-		if w, err := logger.NewErrorWriter(logCnf, debug); err == nil {
-			wts = append(wts, w)
-		} else {
-			return nil, err
-		}
-
-	}
-	if debug {
-		wts = append(wts, writer.NewStdWriter())
-	}
-
-	return multiwriter.New(wts...), nil
 }
